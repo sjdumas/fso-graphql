e36eb4ff76ffbaa68d433d00b731cd51b8bc45c3
S. Dumas <me@sjdumas.comm>
2026-09-01 18:20:50 -0400

    feat: add mutation for addBook

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
 	{
@@ -119,6 +120,15 @@ const typeDefs = /* GraphQL */`
 		allBooks(author: String, genre: String): [Book!]!
 		allAuthors: [Author!]!
 	}
+
+	type Mutation {
+		addBook(
+			title: String!
+			author: String!
+			published: Int!
+			genres: [String!]!
+		): Book!
+	}
 `;
 
 const resolvers = {
@@ -146,6 +156,21 @@ const resolvers = {
 		bookCount: (root) => 
 			books.filter((book) => book.author === root.name).length,
 	},
+	Mutation: {
+		addBook: (root, args) => {
+			const book = { ...args, id: uuid() };
+			books = books.concat(book);
+
+			const authorExists = authors.some((a) => a.name === args.author);
+
+			if (!authorExists) {
+				const newAuthor = { name: args.author, id: uuid() };
+				authors = authors.concat(newAuthor);
+			}
+
+			return book;
+		}
+	}
 };
 
 const server = new ApolloServer({
