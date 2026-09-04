15e95557fcf13123cde875d10fcf78f8c58dbfef
S. Dumas <me@sjdumas.comm>
2026-09-03 20:01:11 -0400

    feat: implement Authors view

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,30 +1,40 @@
+import { useQuery } from "@apollo/client/react";
+import { ALL_AUTHORS } from "../queries";
+
 const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+	const result = useQuery(ALL_AUTHORS);
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.id}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
-}
+	if (!props.show) {
+		return null;
+	}
+	
+	if (result.loading) {
+		return <div>loading...</div>
+	}
 
-export default Authors
+	const authors = result.data.allAuthors;
+
+	return (
+		<div>
+			<h2>authors</h2>
+			<table>
+				<tbody>
+					<tr>
+						<th></th>
+						<th>born</th>
+						<th>books</th>
+					</tr>
+					{authors.map((a) => (
+						<tr key={a.id}>
+							<td>{a.name}</td>
+							<td>{a.born}</td>
+							<td>{a.bookCount}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default Authors;
